7b7d3bdb6e1a335f39fc77557d2d246e481f60e7
Inbloom9 <108229307+Inbloom9@users.noreply.github.com>
2022-06-28 23:15:19 +0800

    Create run.py

---
+++ run.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from webcam import webcam
+import av
+
+captured_image = webcam()
+
+def recv(self, frame):
+    # 将图片转成 Numpy 数组
+    img = frame.to_ndarray(format="bgr24")
+    # 边缘检测算子
+    img = cv2.cvtColor(cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR)
+    # .recv()的输出将显示在屏幕上
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+if captured_image is None:
+    st.write("Waiting for capture...")
+else:
+    st.write("Got an image from the webcam:")
+    img = recv(captured_image)
+    st.image(img)
+    
