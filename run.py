98326af44ad395b646b1f77b165ce9b40de4d634
Inbloom9 <108229307+Inbloom9@users.noreply.github.com>
2022-06-28 23:39:54 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 import streamlit as st
 from webcam import webcam
 import av
+import cv2
 
-captured_image = webcam()
-
-def recv(self, frame):
+def recv(frame):
     # 将图片转成 Numpy 数组
     img = frame.to_ndarray(format="bgr24")
     # 边缘检测算子
@@ -12,11 +11,12 @@ def recv(self, frame):
     # .recv()的输出将显示在屏幕上
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
+captured_image = webcam()
+
 if captured_image is None:
     st.write("Waiting for capture...")
 else:
     st.write("Got an image from the webcam:")
-    
-    image = recv(captured_image)
-    st.image(image)
+    img = recv(captured_image)
+    st.image(img)
     
