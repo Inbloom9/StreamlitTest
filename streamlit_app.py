db7d39fe77e4588618d3496f21f4bcc7fe24b287
Inbloom9 <108229307+Inbloom9@users.noreply.github.com>
2022-07-16 11:51:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ ctx = webrtc_streamer(
     mode=WebRtcMode.SENDRECV,
     video_processor_factory=VideoProcessor,
     rtc_configuration={  # Add this line
-        "iceServers": [{"urls": ["stun:stun.voiparound.com:3478"]}]
+        "iceServers": [{"urls": ["stun:stun.voipbuster.com:3478"]}]
     },
     async_processing=True
 )
